ad9d0fb142b1f113a684892ef8f24a0ecc0d32d1
Alberto Villa <alberto.villa@facile.it>
2021-06-18 13:52:08 +0200

    Add function to convert invalid dates to optional dates

--- src/Date.ts
+++ src/Date.ts
@@ -1,11 +1,11 @@
-import { option } from 'fp-ts'
 import { pipe } from 'fp-ts/function'
-import { Option } from 'fp-ts/Option'
+import * as O from 'fp-ts/Option'
 
-export const fromString = (date: string): Option<Date> =>
-  pipe(new Date(date), (date) =>
-    isFinite(date as any) ? option.some(date) : option.none,
-  )
+export const fromDate = (date: Date): O.Option<Date> =>
+  isFinite(date as any) ? O.some(date) : O.none
 
-export const parse = (date: string): Option<number> =>
-  pipe(date, Date.parse, option.of, option.filter(isFinite))
+export const fromString = (date: string): O.Option<Date> =>
+  pipe(new Date(date), fromDate)
+
+export const parse = (date: string): O.Option<number> =>
+  pipe(date, Date.parse, O.of, O.filter(isFinite))
