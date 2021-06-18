0daa273ce87f15c549a748257b6cc73249e8c839
Alberto Villa <alberto.villa@facile.it>
2021-06-18 13:52:15 +0200

    Update lib/

--- lib/Date.d.ts
+++ lib/Date.d.ts
@@ -1,3 +1,4 @@
-import { Option } from 'fp-ts/Option';
-export declare const fromString: (date: string) => Option<Date>;
-export declare const parse: (date: string) => Option<number>;
+import * as O from 'fp-ts/Option';
+export declare const fromDate: (date: Date) => O.Option<Date>;
+export declare const fromString: (date: string) => O.Option<Date>;
+export declare const parse: (date: string) => O.Option<number>;
