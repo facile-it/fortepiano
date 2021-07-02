b862fd794ae80aed87fa57b6830866f6ad6ea4c1
Alberto Villa <alberto.villa@facile.it>
2021-07-03 01:33:46 +0200

    Update lib/

--- lib/Cache.d.ts
+++ lib/Cache.d.ts
@@ -8,7 +8,7 @@ export interface Cache {
         <C extends t.Mixed>(key: string, codec: C): TE.TaskEither<Error, t.TypeOf<C>>;
         (key: string): TE.TaskEither<Error, unknown>;
     };
-    readonly set: (key: string, value: unknown) => TE.TaskEither<Error, void>;
+    readonly set: (key: string) => (value: unknown) => TE.TaskEither<Error, void>;
     readonly delete: (key: string) => TE.TaskEither<Error, void>;
     readonly clear: TE.TaskEither<Error, void>;
 }
