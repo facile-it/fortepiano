4385ee8944d01028b7414de35826869c5b60873e
Alberto Villa <alberto.villa@facile.it>
2021-07-05 00:45:30 +0200

    Add chain cache

--- src/Cache.ts
+++ src/Cache.ts
@@ -1,4 +1,6 @@
+import { constVoid, pipe } from 'fp-ts/function'
 import * as J from 'fp-ts/Json'
+import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray'
 import * as RR from 'fp-ts/ReadonlyRecord'
 import * as TE from 'fp-ts/TaskEither'
 import * as t from 'io-ts'
@@ -22,5 +24,32 @@ export interface Cache {
 }
 
 export type HasCache<K extends string = 'cache'> = RR.ReadonlyRecord<K, Cache>
+
+export const chain = (...caches: RNEA.ReadonlyNonEmptyArray<Cache>): Cache => ({
+  get: (key: string, codec = t.unknown) =>
+    pipe(
+      caches,
+      RNEA.reduce(TE.left<Error, unknown>(Error()), (value, { get }) =>
+        TE.Alt.alt(value, () => get(key, codec)),
+      ),
+    ),
+  set: (key, ttl) => (value) =>
+    pipe(
+      caches,
+      TE.traverseArray(({ set }) => set(key, ttl)(value)),
+      TE.map(constVoid),
+    ),
+  delete: (key) =>
+    pipe(
+      caches,
+      TE.traverseArray(({ delete: _delete }) => _delete(key)),
+      TE.map(constVoid),
+    ),
+  clear: pipe(
+    caches,
+    TE.traverseArray(({ clear }) => clear),
+    TE.map(constVoid),
+  ),
+})
 
 export { memory, storage }
