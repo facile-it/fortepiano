d30889d6a2e02bcb2e47aaa2cea981c682d1a558
Alberto Villa <alberto.villa@facile.it>
2021-07-05 14:16:43 +0200

    Test HTTP cache client

---
+++ src/Http.test.ts
@@ -0,0 +1,30 @@
+import * as E from 'fp-ts/Either'
+import { memory } from './Cache'
+import { cache, mock } from './Http'
+
+describe('Http', () => {
+  describe('cache', () => {
+    const _cache = memory()
+    const http = cache(_cache)(mock)
+
+    describe('get', () => {
+      it('should return the same successful result', async () => {
+        const h = await http.get('foo')()
+        const c = await http.get('foo')()
+
+        if (E.isRight(h)) {
+          expect(c).toStrictEqual(h)
+        } else {
+          expect(h).not.toStrictEqual(c)
+        }
+      })
+      it('should return different results when the cache is cleared', async () => {
+        const h = await http.get('foo')()
+        await _cache.clear()
+        const c = await http.get('foo')()
+
+        expect(c).not.toStrictEqual(h)
+      })
+    })
+  })
+})
