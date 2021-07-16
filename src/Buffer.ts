dcb040b25c8800173a44f738045e8e0f72b27d4b
Alberto Villa <alberto.villa@facile.it>
2021-07-16 10:17:19 +0200

    Add function to read a stream into a buffer

--- src/Buffer.ts
+++ src/Buffer.ts
@@ -1,4 +1,6 @@
 import * as t from 'io-ts'
+import * as $E from './Error'
+import * as $TE from './TaskEither'
 
 const is = (u: unknown): u is Buffer => u instanceof Buffer
 
@@ -23,3 +25,15 @@ export const BufferFromStringC = new t.Type(
   },
   BufferC.encode,
 )
+
+export const fromStream = (stream: NodeJS.ReadableStream) =>
+  $TE.tryCatch(
+    () =>
+      new Promise<Buffer>((resolve, reject) => {
+        const buffer: Array<any> = []
+        stream.on('error', reject)
+        stream.on('data', (data) => buffer.push(data))
+        stream.on('end', () => resolve(Buffer.concat(buffer)))
+      }),
+    $E.fromUnknown(Error('Cannot read stream into buffer')),
+  )
