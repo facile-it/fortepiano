1c0f4bc45c7ce2a0a84ff013c10e4866ac2a9f62
Alberto Villa <alberto.villa@facile.it>
2021-07-16 10:17:39 +0200

    Update lib/

--- lib/Buffer.d.ts
+++ lib/Buffer.d.ts
@@ -2,3 +2,4 @@
 import * as t from 'io-ts';
 export declare const BufferC: t.Type<Buffer, string, unknown>;
 export declare const BufferFromStringC: t.Type<Buffer, string, unknown>;
+export declare const fromStream: (stream: NodeJS.ReadableStream) => import("fp-ts/lib/TaskEither").TaskEither<Error, Buffer>;
