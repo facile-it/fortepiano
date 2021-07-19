a697f814b0f72c653bd319092d5973c7afcc9daa
Alberto Villa <alberto.villa@facile.it>
2021-07-19 15:00:41 +0200

    Update lib/

--- lib/cache/Redis.d.ts
+++ lib/cache/Redis.d.ts
@@ -1,4 +1,4 @@
 import { Lazy } from 'fp-ts/function';
 import { RedisClient } from 'redis';
 import * as $C from '../Cache';
-export declare const $redis: (redis: Lazy<RedisClient>, ttl?: number) => $C.Cache;
+export declare const $redis: (redis: Lazy<Promise<RedisClient>>, ttl?: number) => $C.Cache;
