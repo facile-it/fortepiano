baccaeb6cd97588f58422576e2d9b77d3c7fe37e
Alberto Villa <alberto.villa@facile.it>
2021-07-23 10:30:19 +0200

    Update lib/

--- lib/Crypto.d.ts
+++ lib/Crypto.d.ts
@@ -1,3 +1,5 @@
 /// <reference types="node" />
 import * as TE from 'fp-ts/TaskEither';
-export declare const hash: (encoding?: BufferEncoding, size?: number) => TE.TaskEither<Error, string>;
+export declare const randomBytes: (size: number) => TE.TaskEither<Error, Buffer>;
+export declare const hash: (size: number, encoding?: BufferEncoding) => TE.TaskEither<Error, string>;
+export declare const uuid4: TE.TaskEither<Error, string>;
