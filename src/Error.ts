ae9f121de8f35dd23bbc130d03391ae608fed77c
Alberto Villa <alberto.villa@facile.it>
2021-07-28 11:58:47 +0200

    Handle io-ts errors

--- src/Error.ts
+++ src/Error.ts
@@ -1,6 +1,7 @@
 import { flow, pipe } from 'fp-ts/function'
-import * as $O from 'fp-ts/Option'
+import * as O from 'fp-ts/Option'
 import * as t from 'io-ts'
+import { failure } from 'io-ts/PathReporter'
 import * as $S from './struct'
 
 const is = (u: unknown): u is Error =>
@@ -18,14 +19,23 @@ export const ErrorC = new t.Type(
   $S.lookup('message'),
 )
 
-const _fromUnknown = (u: unknown) =>
-  // eslint-disable-next-line no-nested-ternary
-  ErrorC.is(u) ? $O.some(u) : t.string.is(u) ? $O.some(Error(u)) : $O.none
+const _fromUnknown = (u: unknown) => {
+  try {
+    // eslint-disable-next-line no-nested-ternary
+    return ErrorC.is(u)
+      ? O.some(u)
+      : t.string.is(u)
+      ? O.some(Error(u))
+      : O.some(Error(failure(u as any).join('\n')))
+  } catch (_) {
+    return O.none
+  }
+}
 
 export const fromUnknown = (e: Error) =>
   flow(
     _fromUnknown,
-    $O.getOrElse(() => e),
+    O.getOrElse(() => e),
   )
 
 const withPrev = (prev: Error) => (error: Error) => {
@@ -40,7 +50,7 @@ export const wrap =
     pipe(
       u,
       _fromUnknown,
-      $O.match(
+      O.match(
         () => e,
         (error) => withPrev(error)(e),
       ),
