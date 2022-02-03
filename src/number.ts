10474144e9984d6974c4a77d7e8d413242de58d4
Alberto Villa <alberto.villa@facile.it>
2022-02-03 01:06:53 +0100

    Fix rounding function

--- src/number.ts
+++ src/number.ts
@@ -4,7 +4,13 @@ const approx =
   (method: 'floor' | 'round' | 'ceil') =>
   (digits = 0) =>
   (a: number): number =>
-    pipe(10 ** digits, (power) => Math[method](a * power) / power)
+    pipe(
+      10 ** digits,
+      (power) =>
+        Math[method](
+          (digits < 0 ? a : Math.sign(a) * Number.EPSILON + a) * power,
+        ) / power,
+    )
 
 export const floor = approx('floor')
 export const round = approx('round')
