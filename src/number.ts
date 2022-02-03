fc494d7479ccf0b42415f44dffd13cf318ba2cef
Alberto Villa <alberto.villa@facile.it>
2022-02-03 11:22:37 +0100

    Add back the string-based implementation of rounding

--- src/number.ts
+++ src/number.ts
@@ -5,11 +5,15 @@ const approx =
   (digits = 0) =>
   (a: number): number =>
     pipe(
-      10 ** digits,
-      (power) =>
-        Math[method](
-          (digits < 0 ? a : Math.sign(a) * Number.EPSILON + a) * power,
-        ) / power,
+      a.toString().split('e'),
+      ([mantissa, exponent]) =>
+        `${mantissa}e${(exponent ? Number(exponent) : 0) + digits}`,
+      (s) => Math[method](Number(s)),
+      (n) => n.toString().split('e'),
+      ([mantissa, exponent]) =>
+        `${mantissa}e${(exponent ? Number(exponent) : 0) - digits}`,
+      Number,
+      (n) => Math.sign(a) * Math.abs(n), // Helps when rounding -0.
     )
 
 export const floor = approx('floor')
