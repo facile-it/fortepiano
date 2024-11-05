7a3966d49843476b4bf5eaaf519c39a3de4e1453
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2024-11-05 18:15:01 +0100

    Remove cartesian, words and anyElem from ReadonlyArray module

--- src/ReadonlyArray.test.ts
+++ src/ReadonlyArray.test.ts
@@ -1,9 +1,8 @@
 import * as E from 'fp-ts/Eq'
 import { pipe } from 'fp-ts/function'
-import * as RA from 'fp-ts/ReadonlyArray'
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray'
 import * as S from 'fp-ts/string'
-import { anyElem, cartesian, same, words } from './ReadonlyArray'
+import { same } from './ReadonlyArray'
 
 const arrays: ReadonlyArray<
   [
@@ -47,96 +46,6 @@ const arrays: ReadonlyArray<
 ]
 
 describe('ReadonlyArray', () => {
-  describe('cartesian', () => {
-    it('should compute cartesian product of given arrays', () => {
-      arrays.forEach(([[head, ...tail], output]) =>
-        expect(cartesian(head, ...tail)).toStrictEqual(output),
-      )
-    })
-  })
-
-  describe('words', () => {
-    it('should return no word for an empty alphabet', () => {
-      expect(pipe([], words(1))).toStrictEqual([])
-      expect(pipe([], words(2))).toStrictEqual([])
-    })
-    it('should support alphabets of one element', () => {
-      expect(pipe([0], words(1))).toStrictEqual([[0]])
-      expect(pipe([0], words(2))).toStrictEqual([[0, 0]])
-    })
-    it('should return words of specified sizes over the given alphabet', () => {
-      const binary = [0, 1] as const
-      /**
-       * {@link https://en.wikipedia.org/wiki/Rock_paper_scissors#Additional_weapons}
-       */
-      const shapes = ['Rock', 'Paper', 'Scissors', 'Spock', 'Lizard'] as const
-      const win = (a: number, b: number): boolean =>
-        Boolean({ 1: true, 3: true }[(5 + a - b) % 5])
-
-      expect(pipe(binary, words(1))).toStrictEqual([[0], [1]])
-      expect(pipe(binary, words(2))).toStrictEqual([
-        [0, 0],
-        [0, 1],
-        [1, 0],
-        [1, 1],
-      ])
-      expect(pipe(binary, words(3))).toStrictEqual([
-        [0, 0, 0],
-        [0, 0, 1],
-        [0, 1, 0],
-        [0, 1, 1],
-        [1, 0, 0],
-        [1, 0, 1],
-        [1, 1, 0],
-        [1, 1, 1],
-      ])
-      expect(
-        pipe(
-          shapes,
-          RA.mapWithIndex((i, a) => [i, a] as const),
-          words(2),
-          RA.filter(([[a], [b]]) => win(a, b)),
-          RA.map(([[, a], [, b]]) => `${a} defeats ${b}`),
-        ),
-      ).toStrictEqual([
-        'Rock defeats Scissors',
-        'Rock defeats Lizard',
-        'Paper defeats Rock',
-        'Paper defeats Spock',
-        'Scissors defeats Paper',
-        'Scissors defeats Lizard',
-        'Spock defeats Rock',
-        'Spock defeats Scissors',
-        'Lizard defeats Paper',
-        'Lizard defeats Spock',
-      ])
-    })
-    it('should support returning the empty word', () => {
-      expect(pipe([0, 1], words(-Infinity))).toStrictEqual([[]])
-    })
-  })
-
-  describe('anyElem', () => {
-    it('should return false when the array has no elements', () => {
-      expect(anyElem(S.Eq)('a')([])).toBe(false)
-    })
-    it('should return false when all elements are missing', () => {
-      expect(anyElem(S.Eq)('a')(['b'])).toBe(false)
-    })
-    it('should return true when some elements are found', () => {
-      expect(anyElem(S.Eq)('a', 'b')(['a'])).toBe(true)
-    })
-    it('should return true when all elements are found', () => {
-      expect(anyElem(S.Eq)('a', 'b')(['b', 'a'])).toBe(true)
-    })
-    it('should return true when all elements are found along with others', () => {
-      expect(anyElem(S.Eq)('a', 'b')(['c', 'b', 'a'])).toBe(true)
-    })
-    it('should return true when duplicate elements are found', () => {
-      expect(anyElem(S.Eq)('a', 'a', 'b')(['b', 'b', 'a', 'a', 'a'])).toBe(true)
-    })
-  })
-
   describe('same', () => {
     it('should return true on empty arrays', () => {
       expect(same(S.Eq)([])).toBe(true)
